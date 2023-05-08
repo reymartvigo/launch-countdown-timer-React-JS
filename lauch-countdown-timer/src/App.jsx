365a9a11593352468729a742c2606172c4382c80
Reymart <vigoreymart11@gmail.com>
2023-05-07 20:20:53 -1000

    Second Commit

--- lauch-countdown-timer/src/App.jsx
+++ lauch-countdown-timer/src/App.jsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="countdown-container">
+        <div className="text-wrapper">
+          <h1>WE'RE LAUNCHING SOON</h1>
+        </div>
+
+        <div className='time-wrapper'>
+          <div className="digit-container">
+            <div className="digits">08</div>
+            <div className="digit-label">DAYS</div>
+          </div>
+
+          <div className="digit-container">
+            <div className="digits">23</div>
+            <div className="digit-label">HOURS</div>
+          </div>
+
+          <div className="digit-container">
+            <div className="digits">55</div>
+            <div className="digit-label">MINUTES</div>
+          </div>
+
+          <div className="digit-container">
+            <div className="digits">41</div>
+            <div className="digit-label">SECONDS</div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
